c65a76b5bc7695d586a5c46edf2e8b8318613844
Ing. Victor Suarez <victor.suarez.is@gmail.com>
2020-10-26 17:26:45 -0400

    Plugin to extend Allocation accounting of iDempiere ERP

---
+++ src/dev/vsuarez/component/DocFactory.java
@@ -0,0 +1,88 @@
+/**
+ * 
+ */
+package dev.vsuarez.component;
+
+import java.lang.reflect.Constructor;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.logging.Level;
+
+import org.adempiere.base.IDocFactory;
+import org.compiere.acct.Doc;
+import org.compiere.acct.Doc_AllocationHdrVS;
+import org.compiere.model.I_C_AllocationHdr;
+import org.compiere.model.MAcctSchema;
+import org.compiere.model.MTable;
+import org.compiere.util.AdempiereUserError;
+import org.compiere.util.CLogger;
+import org.compiere.util.DB;
+import org.compiere.util.Env;
+
+/**
+ * @author <a href="mailto:victor.suarez.is@gmail.com">Ing. Victor Suarez</a>
+ *
+ */
+public class DocFactory implements IDocFactory {
+	
+	private final static CLogger s_log = CLogger.getCLogger(DocFactory.class);
+
+	@Override
+	public Doc getDocument(MAcctSchema as, int AD_Table_ID, int Record_ID, String trxName) {
+		String tableName = MTable.getTableName(Env.getCtx(), AD_Table_ID);
+		if (!tableName.equals(I_C_AllocationHdr.Table_Name))
+			return null;
+		//
+		Doc doc = null;
+		StringBuffer sql = new StringBuffer("SELECT * FROM ")
+			.append(tableName)
+			.append(" WHERE ").append(tableName).append("_ID=? AND Processed='Y'");
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try
+		{
+			pstmt = DB.prepareStatement (sql.toString(), trxName);
+			pstmt.setInt (1, Record_ID);
+			rs = pstmt.executeQuery ();
+			if (rs.next ())
+			{
+				doc = getDocument(as, AD_Table_ID, rs, trxName);
+			}
+			else
+				s_log.severe("Not Found: " + tableName + "_ID=" + Record_ID);
+		}
+		catch (Exception e)
+		{
+			s_log.log (Level.SEVERE, sql.toString(), e);
+		}
+		finally
+		{
+			DB.close(rs, pstmt);
+			rs = null;
+			pstmt = null;
+		}
+		return doc;
+	}
+
+	@Override
+	public Doc getDocument(MAcctSchema as, int AD_Table_ID, ResultSet rs, String trxName) {
+		Doc doc = null;
+		String className = Doc_AllocationHdrVS.class.getCanonicalName();
+		try
+		{
+			Class<?> cClass = Class.forName(className);
+			Constructor<?> cnstr = cClass.getConstructor(new Class[] {MAcctSchema.class, ResultSet.class, String.class});
+			doc = (Doc) cnstr.newInstance(as, rs, trxName);
+		}
+		catch (Exception e)
+		{
+			s_log.log(Level.SEVERE, "Doc Class invalid: " + className + " (" + e.toString() + ")");
+			throw new AdempiereUserError("Doc Class invalid: " + className + " (" + e.toString() + ")");
+		}
+
+		if (doc == null)
+			s_log.log(Level.SEVERE, "Unknown AD_Table_ID=" + AD_Table_ID);
+		return doc;
+	}
+
+}
